4effbb987a706baa51d3653cbd3353cc135ec799
Andrew Oliveira <andrewhenriquef@gmail.com>
2021-07-31 13:28:46 -0300

    (-) Remove unused route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,3 @@
 class HomeController < ApplicationController
   def index; end
-
-  def download
-    service = YoutubeDlWrapperService.new(
-      youtube_url: allowed_parameters[:youtube_url]
-    )
-
-    service.download
-
-    file_name = service.file_name
-    file_path = service.file_path
-
-    File.open(file_path) do |music_file|
-      ActionCable.server.broadcast(
-        "download_music_channel_#{uuid}",
-        music: music_file,
-        file_name: file_name,
-        content_type: 'audio/mpeg'
-      )
-    end
-  end
-
-  private
-
-  def allowed_parameters
-    params.permit(:youtube_url)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home/index'
-  get 'home/download'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
