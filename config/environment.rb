250f06809a323732d0f559ae5530d1560e29ccca
Andrew Oliveira <andrewhenriquef@gmail.com>
2021-07-31 15:33:26 -0300

    (+) Setup redis to perform caching

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.cache_store = :redis_cache_store, { url: Rails.application.credentials.dig(:redis, :url) }
+end
